0535276498e2de7205c997f32ba0c63f844ee716
aakashmalhotra22 <aakashmalhotra2863@gmail.com>
2023-01-09 12:09:17 +0530

    adding files

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from '@mui/material/Button';
+
+let sname = "aakash";
+function App() 
+{
+  return (
+    <>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      {/* <h1 className="a1">This is head</h1>
+      <div className='d1'>This is a div</div>
+      <h2>hello {sname}</h2> */}
+    </>
+    
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
